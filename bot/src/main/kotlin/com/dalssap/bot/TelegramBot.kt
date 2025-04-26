d02b0321b655a9af88c96715c1b12cd32c3cff40
dalssap <dalssap99@gmail.com>
2025-04-26 17:21:31 +0900

    [FEAT] telegram message polling

--- bot/src/main/kotlin/com/dalssap/bot/TelegramBot.kt
+++ bot/src/main/kotlin/com/dalssap/bot/TelegramBot.kt
@@ -1,1 +1,54 @@
 package com.dalssap.bot
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Update
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException
+import org.telegram.telegrambots.meta.generics.TelegramClient
+
+@Component
+class TelegramBot(
+    @Value("\${telegram.bot.token}")
+    private val botToken: String,
+    private val telegramClient: TelegramClient
+): SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+
+    override fun getBotToken() = botToken
+
+    override fun getUpdatesConsumer() = this
+
+    override fun consume(update: Update) {
+        if (!update.hasMessage()) {
+            return
+        }
+
+        try {
+            telegramClient.execute(
+                SendMessage.builder()
+                    .chatId(update.message.chatId)
+                    .text("애옹")
+                    .build()
+            )
+        } catch (e: TelegramApiException) {
+            e.printStackTrace()
+        }
+    }
+
+}
+
+@Configuration
+class TelegramClientConfig(
+    @Value("\${telegram.bot.token}") private val botToken: String,
+) {
+
+    @Bean
+    fun telegramClient(): TelegramClient {
+        return OkHttpTelegramClient(botToken)
+    }
+}
